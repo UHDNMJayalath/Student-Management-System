e6429b62f9174a54933743ece28926585f62dca5
UHDNMJayalath <nishakamahesh2020@gmail.com>
2025-02-07 16:50:54 +0530

    Initial commit

---
+++ Student Management System/src/Student.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+public class Student {
+    int id;
+    String name;
+    int age;
+
+    public Student(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    void display(){
+        System.out.println("ÏD:"+id);
+        
+    }
+}
