b339ca39a1117d9e5b3e5cb09ea45989a74d82db
UHDNMJayalath <nishakamahesh2020@gmail.com>
2025-02-09 20:58:39 +0530

    Student

--- Student Management System/src/Student.java
+++ Student Management System/src/Student.java
@@ -13,6 +13,36 @@ public class Student {
 
     void display(){
         System.out.println("ÏD:"+id);
-        
+        System.out.println("Name:"+name);
+        System.out.println("Age:"+age);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    @Override
+    public String toString() {
+        return "ID: " + id + ", Name: " + name + ", Age: " + age;
     }
 }
+
